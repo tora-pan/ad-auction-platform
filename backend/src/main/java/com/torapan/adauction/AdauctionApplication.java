7da48380f4512beb18913302f74673b335c1ad6c
Travis Pandos <tora@Mac.lan>
2026-02-14 15:18:36 -0800

    download backend deps

---
+++ backend/src/main/java/com/torapan/adauction/AdauctionApplication.java
@@ -0,0 +1,13 @@
+package com.torapan.adauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdauctionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/torapan/adauction/AdauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torapan.adauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
